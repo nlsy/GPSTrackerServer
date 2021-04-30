2453b426c1634e018bce091b09da8ee0c32722a9
nlsy <58358315+nlsy@users.noreply.github.com>
2021-04-30 13:39:58 +0200

    Update caTracker.php

--- src/caTracker.php
+++ src/caTracker.php
@@ -3,15 +3,16 @@
  *  Contains the structure of the Tracker-Object
  */
 
-class caTracker{
-  private $key;   // Tracker key
-  private $time;  // Satellite Time
-  private $lon;   // GPS Longitude
-  private $lat;   // GPS Latitude
-  private $gsm;   // GSM-Localization
-  private $moni;  // MONI
+class Tracker{
+  private $serial;    // Tracker key
+  private $datetime;  // Server DateTime
+  private $gpstime;   // Satellite Time
+  private $lon;       // GPS Longitude (decimal degrees - World Geodetic System 84 (WGS 84))
+  private $lat;       // GPS Latitude (decimal degrees - World Geodetic System 84 (WGS 84))
+  private $gsm;       // GSM-Localization
+  private $moni;      // MONI (#MONI: Cell BSIC LAC CellId ARFCN Power C1 C2 (TaRxQual PLMN))
 
-  public function __construct(string $input){
+  public function __construct($input){
     $this->gsm = '';
   }
   public function getGSM(){
