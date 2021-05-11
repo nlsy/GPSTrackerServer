0c679634bddbd55275bfa693738acfe325dc4791
nlsy <58358315+nlsy@users.noreply.github.com>
2021-05-11 22:32:22 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -5,12 +5,13 @@
 require_once('config.php');
 require_once('caTracker.php');
 
-// header('Location:index.php');
+// header("Location:index.php");
 
 // Receiver example
-// header('Location:receiver.php?');
+// header("Location:receiver.php?");
+// header("Location:receiver.php?gpstime=111418.0&lon=47G48'29.4''N&lat=9G38'43.8''E&gsm=5 45 1 0");
 
 // Mapping example
-// header('Location:map.php?key=');
+// header("Location:map.php?key=");
 
 ?>
