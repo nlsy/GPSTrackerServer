de4f9c4f6f3cd3df805f85ac1a8f62cefb52dae4
nlsy <58358315+nlsy@users.noreply.github.com>
2021-05-30 12:26:55 +0200

    Minor changes

--- src/caTracker.php
+++ src/caTracker.php
@@ -10,7 +10,6 @@ class Tracker{
   private $lon;       // GPS Longitude (decimal degrees - World Geodetic System 84 (WGS 84))
   private $lat;       // GPS Latitude (decimal degrees - World Geodetic System 84 (WGS 84))
   private $gsm;       // GSM-Localization
-  private $moni;      // MONI (#MONI: Cell BSIC LAC CellId ARFCN Power C1 C2 (TaRxQual PLMN))
   
   // Latitude N (using S result has to be negated)
   // Longitude E (using W result has to be negated)
