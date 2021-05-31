af4940e24383f02c23ec713b6ef376145b620cc9
nlsy <58358315+nlsy@users.noreply.github.com>
2021-06-01 00:30:34 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,13 +3,11 @@
  *  Function test of the server
  */
 
-// header("Location:index.php");
-
 // Receiver example (uncomment to try)
 //header("Location:receiver.php?key=&gpstime=111418.0&lon=9G38'43.8''E&lat=47G48'29.4''N&gsm=5 45 1 0 E-Plus; MONI: N1 77 E720 E796 980 -74dbm 25 19; MONI: N2 70 E720 98A4 685 -75dbm 30 24; MONI: N3 74 E720 4B56 982 -79dbm 20 14; MONI: N4 36 E720 4D2E 669 -81dbm 24 18; MONI: N5 75 E720 9A7F 690 -82dbm 23 17; MONI: N6 31 E720 8B39 984 -87dbm 18 12;");
 
 // Provider example (uncomment to try)
-//header("Location:provider.php?key=");
+header("Location:provider.php?key=");
 
 // Show map (uncomment to try)
 //header("Location:map.html");
